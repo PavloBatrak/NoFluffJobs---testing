842b950f9ba7716dba912894a16469127f1c1295
Pavlo <batrak.p@gmail.com>
2021-01-25 22:44:47 +0100

    Count web elements from tabs: Cennik, Klienci, Wydarzenia

--- src/test/java/pages/Employers.java
+++ src/test/java/pages/Employers.java
@@ -9,23 +9,39 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class Employers extends Base {
-    
+
     private WebElement changeEmployeePage;
     private WebElement checkEmployeePage;
     private WebElement priceTab;
+    private WebElement countPriceTabElements;
+    private WebElement countClientsTabElements;
+    private WebElement countEventsTabElements;
 
 
-    public Employers(WebDriver driver, WebDriverWait wait){
+    public Employers(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
     }
 
-    public void switchToTheEmployeePage(){
+    public void switchToTheEmployeePage() {
         changeEmployeePage = driver.findElement(By.xpath("//a[@href][contains(., 'dla pracodawcy')]"));
         changeEmployeePage.click();
-        // click price tab
         priceTab = driver.findElement(By.xpath("//div[@id='sub-navigation']//li/a[contains(., 'Cennik')]"));
         priceTab.click();
+
+        //count number of elements on PRICE tab
+        // webdriver.findElements(By.xpath("required_XPATH")).size();
+        int countPriceTabElements = driver.findElements(By.cssSelector("body > div:nth-child(6) > div.nfj-page-content.rebranding-pricing-page.primary-variant > section.pricing-header > div.nfj-pricing-table > article.offer-box.position-relative")).size();
+
+        /*List<WebElement> countPriceTabElements = driver.findElements(By.xpath("*"));
+        System.out.println(countPriceTabElements);*/
+
+        // & count number of elements on CLIENTS tab
+        int countClientsTabElements = driver.findElements(By.xpath("//div[@class=\"col-lg-4 mb-5\"]")).size();
+
+        // & count number of elements on EVENTS tab
+        int countEventsTabElements = driver.findElements(By.xpath("//div[@style=\"opacity: 1;\"]")).size();
+
 
         //i need to return numbers of elements from "cennik"
 
@@ -40,7 +56,7 @@ public class Employers extends Base {
 
     }
 
-    public boolean isNoFluffEmployee(){
+    public boolean isNoFluffEmployee() {
         changeEmployeePage = driver.findElement(By.xpath("//a[@href][contains(., 'dla pracodawcy')]"));
         changeEmployeePage.click();
         // po wykorzystaniu switchToTheEmployeePage satrona ma inny xpath!! temu changeEmployeePage nie pracuje
